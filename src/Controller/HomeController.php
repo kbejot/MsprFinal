ef749203c6b306152681e5345705c121d3508520
kbejot <116283599+kbejot@users.noreply.github.com>
2023-07-06 15:19:10 +0200

    finalisation_faq_billeterie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,33 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/welcome', name: 'app_welcome')]
+    public function ifLog(): Response
+    {
+        return $this->render('home/welcome.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/faq', name: 'app_faq')]
+    public function showFAQ(): Response
+    {
+        return $this->render('navigation/faq.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/billeterie', name: 'app_billeterie')]
+    public function showBilleterie(): Response
+    {
+        return $this->render('navigation/billeterie.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/login', name: 'app_login')]
+    public function showLogin(): Response
+    {
+        return $this->render('user/login.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
 }
