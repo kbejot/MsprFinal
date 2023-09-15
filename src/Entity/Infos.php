867027f6d0ec7644e1e38d28d98c238e25f1ade1
unknown <kevin.bejot@gmail.com>
2023-09-15 16:27:09 +0200

    ajout api

--- src/Entity/Infos.php
+++ src/Entity/Infos.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\InfosRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: InfosRepository::class)]
+#[ApiResource]
 class Infos
 {
     #[ORM\Id]
